28c799a1ef88196eb664820c4e0d94c9806ccf3a
stone900 <stonedavid002@gmail.com>
2023-02-16 02:46:04 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "programming is like building a multilingual puzzel
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilinguaul puzzel");
+	return (0);
+}
